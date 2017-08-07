fe2a5f065db0493a79eaaabe444d8de648062637
chenjunfa <1026487348@qq.com>
2017-08-07 21:51:09 +0800

    xx

---
+++ resources/views/autocopy/.blade.php
@@ -0,0 +1,268 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>美妙app—共享主义新生活</title>
+<meta name="description" content="美妙appapp,美妙appapp是一款专为95后设计的聊天交友软件，用户只需要付出时间就能收获朋友，还可以利用自己的兴趣和热情赚钱，将你的才华展现出来，做做任务即可轻松获得收益，赶快下载使用吧！嘻嘻星球app应用介绍：在嘻嘻星球，95后的你有多少工作经验并不重要，只要你肯付出,嘻嘻星球西西最安全的下载地址.">
+<meta name="keywords" content="美妙app, 美妙app, cc, 新生活方式服务平台, superxing, 嘻嘻, 星球, Mobile, iOS, Android">
+<meta name="author" content="美妙app">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<link rel="icon" href="images/favicon.ico">
+<link href="css/style.css" rel="stylesheet">
+
+<style type="text/css">
+.navbar-header1{
+	width: 100%;
+	display: none;
+	 position:fixed;_position:absolute;bottom:0px;_bottom:0px;_margin-top:expression(this.style.pixelHeight+document.documentElement.scrollTop);
+    z-index:9999;
+}
+.nav_box{
+	width: 100%;
+	
+}
+.nav_box img{
+	width: 100%;
+}
+@media only screen and (max-width : 640px) {
+    .navbar-header1{
+	display: block;
+}
+@media only screen and (max-width : 640px) {
+    #divD{
+    display: block;
+}
+}
+
+ #divD{
+        width: 800px;
+        height: 80px;
+    }
+</style>
+
+</head>
+<body id="app" data-spy="scroll" data-target=".navbar-custom">
+<div class="preloader">
+    <div class="status">
+        <img class="img-responsive" src="images/logo-lg.png" alt="加载中..."/>
+    </div>
+</div><!-- loading -->
+<header id="header">
+    <nav class="navbar navbar-custom navbar-top navbar-fixed-top sticky-navigation" >
+        <div class="container">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="icon icon-bar"></i>
+                </button>
+                <a class="navbar-brand nav-logo page-scroll" href="#app">
+                    美妙时光
+                    <!-- <img src="images/lo.pngw" alt="美妙app"/> -->
+                </a><!-- logo -->
+            </div>
+
+            <div class="collapse navbar-collapse navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a class="page-scroll" href="#app">APP</a></li>
+                    <li><a class="page-scroll" href="#about">关于我们</a></li>
+                    <li><a class="page-scroll" href="#contact">联系我们</a></li>
+                </ul>
+            </div>
+            <!-- link -->
+
+        </div>
+    </nav>
+</header><!-- Header end -->
+
+
+<section id="intro" style="z-index:1000">
+
+    <div class="intro-overlay"></div>
+    <!-- overlay end -->
+
+    <div class="container">
+        <div class="row relative">
+            <div class="col-md-4 col-md-offset-1 wow bounceInDown" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <div class="bg-iphone">
+                    <div id="bannerSlider" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img class="img-responsive" src="images/banner/1.jpg" alt="1">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive" src="images/banner/2.jpg" alt="2">
+                            </div>
+                            <div class="item">
+                                <img class="img-responsive" src="images/banner/3.jpg" alt="3">
+                            </div>
+                        </div><!-- Wrapper for slides -->
+                    </div>
+                </div>
+            </div><!-- col-md-6 end -->
+
+            <div class="col-md-4 col-md-offset-1 wow bounceInRight" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+
+                <h1 class="cnhei">美妙app</h1>
+                <p class="space40 c-gray">
+                    共享主义新生活<br>
+                    百万＋有颜、有才、有生活态度的人在这里等你
+                </p>
+
+<!--                 <div class="row">
+                    <div class="col-xs-7">
+                        <div class="downloads" >
+                            <a style="margin-top:0px" class="btn btn-default btn-lg btn-block bhover" href="https://itunes.apple.com/cn/app/cc%E6%98%9F%E7%90%83-%E6%96%B0%E7%94%9F%E6%B4%BB%E6%96%B9%E5%BC%8F%E6%9C%8D%E5%8A%A1%E5%B9%B3%E5%8F%B0/id1207541773?l=en&mt=8"><i class="icon icon-ios"></i> IOS</a>
+                            <a class="btn btn-default btn-lg btn-block bhover" href="http://download.shareapi.cn/packages/ccxingqiu_home_2.2.2.apk"><i class="icon icon-android"></i> Android</a>
+                        </div>
+                    </div>
+                    <div class="qrcode col-xs-5 text-center" >
+                        <img class="img-responsive" src="images/qrcode.png" alt="二维码">
+                        <div class="f12 c-gray">扫码下载，就现在</div>
+                    </div>
+                </div>
+ -->
+            </div><!-- col-md-6 end -->
+
+        </div>
+    </div>
+    <div id="yYy"></div>
+    <div id="ballY"></div>
+    <div id="bally"></div>
+</section><!-- intro end -->
+
+
+<section id="about" class="section">
+    <div class="container">
+
+        <div class="row wow flipInX" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+            <div class="col-md-12">
+                <h2 class="title text-center">
+                    <span class="number1"></span>
+                    <span class="cnhei"><em class="f30">关于我们/</em><em class="f20">About Us</em></span>
+                </h2>
+            </div>
+        </div><!-- row end -->
+
+        <div class="row">
+            <div class="about-img col-md-6 wow bounceInLeft" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <div class="overmask"></div>
+                <img src="images/company.jpg" class="img-responsive" alt="公司介绍" />
+            </div><!-- col-md-4 end -->
+
+            <div class="about-intro col-md-6 wow bounceInRight" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <i class="icon icon-tleft"></i>
+                <p>美妙app是由北京美妙时光科技有限公司开发的专属于95后的交易社交平台，也是国内最大校园红人平台。</p>
+                <p>在这里聚集了百万+有趣、优质、有生活态度的用户；
+                在这里你的魅力和生活态度有人欣赏；
+                在这里寻找兴趣相投、有共同爱好的人的新新人类！</p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+            </div><!-- col-md-4 end -->
+        </div><!-- row end -->
+
+    </div>
+</section><!-- about end -->
+
+
+<section id="contact" class="section">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12 wow flipInX" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <h2 class="title text-center">
+                    <span class="number2"></span>
+                    <span class="cnhei"><em class="f30">联系我们/</em><em class="f20">Contact Us</em></span>
+                </h2>
+            </div><!-- end -->
+
+            <div class="col-md-6 wow bounceInLeft" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <dl>
+                    <dt><i class="icon icon-group"></i> 商务合作</dt>
+                    <dd>欢迎以下企业联系我们：<br>如果您具有娱乐精神，不拘一格、对95后奇葩世界感兴趣，喜欢创新、招贤纳士，想要获取掘金钥匙；或者需要品牌营销、活动推广、资源互换…</dd>
+                    <dd>美妙app欢迎您<br>请邮件至：<b style="color: #000;">business@shareapi.cn</b></dd>
+                </dl>
+            </div><!-- end -->
+
+            <div class="col-md-3 col-md-offset-3 wow bounceInRight" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <dl>
+                    <dt><i class="icon icon-join"></i> 加入我们</dt>
+                    <dd style="font-weight: bold;color: #000;">hr@shareapi.cn</dd>
+                </dl>
+<!--                 <dl>
+                    <dt><i class="icon icon-24"></i> 热线客服</dt>
+                    <dd style="font-weight: bold;color: #000;">010</dd>
+                </dl> -->
+            </div><!-- end -->
+
+            <div class="col-md-12 wow bounceInUp" data-wow-duration="1s" data-wow-offset="50" data-wow-delay="0s">
+                <img class="img-responsive" src="images/map.png" alt="北京市朝阳区草场地艺术区241-8"/>
+            </div><!-- end -->
+
+        </div>
+    </div>
+</section><!-- contact end -->
+
+
+<footer id="footer">
+    <div class="container">
+        <div class="col-md-12 ">
+            <dl>
+                <dt class="cnhei">美妙app</dt>
+                <dd>COPYRIGHT &copy; 2016-2017<br>ALL RIGHTS RESERVED. 京ICP备16034375号</dd>
+            </dl>
+        </div>
+    </div>
+</footer><!-- footer end -->
+<div id="divD"></div>
+
+    <!-- <div ><a href="javascript:;" id="add"><img src="images/screenshot/bg1.png" alt="美妙app"/></a></div> -->
+<!--  <div class="navbar-header">
+    <div id="divA">
+    <div id="divB"> <img src="images/screenshot/bg@3x.png" alt="美妙app"/></div>
+    <div id="divC"><img src="images/screenshot/logo4444.png" alt="美妙app"/></div>
+    </div>
+</div> -->
+<div class="navbar-header1">
+                    <div class='nav_box' id="add">
+                    <a href="javascript:;" id="add">
+                    <img src="images/screenshot/bg1.png" alt="美妙app"/>
+                    </a>
+                    </div>
+</div>
+
+<!-- javascript Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.ui.min.js"></script>
+<script src="js/script.js"></script>
+<script src="js/modernizr.min.js"></script>
+
+<script type="text/javascript">
+$(document).on('click','#add',function(){
+    // alert("ahiafadf");
+var browser = {
+versions: function () {
+var u = navigator.userAgent, app = navigator.appVersion;
+return { //移动终端浏览器版本信息 
+ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端 
+android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器 
+iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器 
+iPad: u.indexOf('iPad') > -1, //是否iPad 
+};
+}(),
+}
+if (browser.versions.iPhone || browser.versions.iPad || browser.versions.ios) {
+//说明是ios系统
+window.location.href="https://itunes.apple.com/cn/app/cc%E6%98%9F%E7%90%83-%E6%96%B0%E7%94%9F%E6%B4%BB%E6%96%B9%E5%BC%8F%E6%9C%8D%E5%8A%A1%E5%B9%B3%E5%8F%B0/id1207541773?l=en&mt=8";
+}
+if (browser.versions.android) {
+//说明是Android系统
+window.location.href="http://download.shareapi.cn/packages/ccxingqiu_home_2.2.2.apk";
+}
+});
+</script>
+</body>
+</html>
